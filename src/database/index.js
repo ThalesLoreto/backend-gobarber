136de7b7fa621136ef42011ec60b0d47387b88f6
ThalesLoreto <thalesmulti3@gmail.com>
2019-09-06 19:30:31 -0300

    create FileController, FileModel, new column in user database && avatar_id sync to user

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import Users from '../app/models/User';
+import Files from '../app/models/Files';
 
 import databaseConfig from '../config/database';
 
-const models = [Users];
+const models = [Users, Files];
 
 class Database {
   constructor() {
@@ -14,7 +15,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
