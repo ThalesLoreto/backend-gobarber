4edc1acaa7bf393f6129c490883cca8c6fb8d119
ThalesLoreto <thalesmulti3@gmail.com>
2019-09-24 12:01:12 -0300

    configuring nodemailer && send a basic email to provider

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8b40c70af61091',
+    pass: '83ba49731682bc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
